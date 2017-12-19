8e049f312397a925033b93ad304fa01bede5e1eb
Chaim Finson <chaim0m@hotmail.com>
2017-12-19 14:31:04 +0200

    final coffeeshop

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 var coffeeShop = {
-  beans: 40,
+ beans: 40,
+ money: 100,
+ bean: 1,
+ drinkRequirements: {
+   latte: {price:5, beanRequirement:10},
+   americano: {price:3,beanRequirement:5},
+   doubleShot: {price:7,beanRequirement:15},
+   frenchPress: {price:8,beanRequirement:12}
+ },
 
-  drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
-  },
-
-  makeDrink: function (drinkType) {
-    // TODO: Finish this method
+ makeDrink: function (drinkType) {
+   if (coffeeShop.drinkRequirements.hasOwnProperty(drinkType) && coffeeShop.beans > coffeeShop.drinkRequirements[drinkType].beanRequirement) {
+       coffeeShop.beans = this.beans - coffeeShop.drinkRequirements[drinkType].beanRequirement;
+     }
+  else if(coffeeShop.beans < coffeeShop.drinkRequirements.hasOwnProperty(drinkType) && coffeeShop.drinkRequirements[drinkType]){
+alert("Sorry, we're all out of beans!")
+     }
+  else {
+    alert("Sorry, we don't make " + drinkType)
+  }
+},
+buySupplies: function(beans){
+  if (this.money>=beans){
+    this.money-=beans;
+  }
+},
+buyDrink: function(drinkType){
+  if (this.drinkRequirements.hasOwnProperty(drinkType)) {
+    this.makeDrink(drinkType);
+    this.money+=this.drinkRequirements[drinkType].price;
   }
 }
+
+};
 
 coffeeShop.makeDrink("latte");
 coffeeShop.makeDrink("americano");
